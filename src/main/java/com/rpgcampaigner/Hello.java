d75aef8c506230b1ac3e744aaa6a633337bd2576
John McCormick <jmccormick@blizzard.com>
2016-09-21 13:51:10 -0700

    stripped everything out to the barest fundamentals

---
+++ src/main/java/com/rpgcampaigner/Hello.java
@@ -0,0 +1,12 @@
+package com.rpgcampaigner;
+
+/**
+ * @author jmccormick
+ * @since 9/21/16
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
